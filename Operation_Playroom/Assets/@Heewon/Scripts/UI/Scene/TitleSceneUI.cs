3cfd03d81f4aea5d752c255a255ec8f897b44255
민희원 <048895@gmail.com>
2025-02-21 17:37:48 +0900

    매치메이킹 버튼에 함수 연결

--- Operation_Playroom/Assets/@Heewon/Scripts/UI/Scene/TitleSceneUI.cs
+++ Operation_Playroom/Assets/@Heewon/Scripts/UI/Scene/TitleSceneUI.cs
@@ -1,6 +1,8 @@
 ﻿using TMPro;
+using Unity.Netcode;
 using Unity.Services.Authentication;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TitleSceneUI : MonoBehaviour
 {
@@ -9,10 +11,21 @@ public class TitleSceneUI : MonoBehaviour
     [SerializeField] TMP_InputField userNameInputField;
     [SerializeField] TMP_InputField joinCodeInputField;
 
+    [SerializeField] TMP_Text findMatchStatusText;
+    [SerializeField] TMP_Text findButtonText;
+
+    bool isMatchmaking;
+    bool isCancelling;
+
     void Init()
     {
         startOptionPanel.SetActive(false);
         lobbyCanvas.SetActive(false);
+
+        if (GameObject.FindFirstObjectByType<NetworkManager>() == null)
+        {
+            SceneManager.LoadScene("NetConnectScene");
+        }
     }
 
     private void Start()
@@ -27,8 +40,45 @@ public class TitleSceneUI : MonoBehaviour
 
     public async void OnFindMatchButtonPressed()
     {
-        startOptionPanel.SetActive(false);
+        if (isMatchmaking)
+        {
+            //cancel
+            isCancelling = true;
+            findMatchStatusText.text = "Cancelling...";
 
+            await ClientSingleton.Instance.CancelMatchmaking();
+
+            isCancelling = false;
+            isMatchmaking = false;
+            findMatchStatusText.text = "";
+            findButtonText.text = "Find Match";
+        }
+        else
+        {
+            //match
+            findMatchStatusText.text = "Searching...";
+            findButtonText.text = "Cancel";
+            isMatchmaking = true;
+            isCancelling = false;
+
+            ClientSingleton.Instance.MatchmakeAsync(OnMatchMade);
+        }
+    }
+
+    void OnMatchMade(MatchmakerPollingResult result)
+    {
+        switch (result)
+        {
+            case MatchmakerPollingResult.Success:
+                findMatchStatusText.text = "connecting...";
+                break;
+            default:
+                isMatchmaking = false;
+                findMatchStatusText.text = "error" + result;
+                findButtonText.text = "Find Match";
+                break;
+        }
+        isMatchmaking = false;
     }
 
     public async void OnChangeUserNameButtonPressed()
