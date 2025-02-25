89c8bd5fa268eca472a55eea511175cf0ea21fa3
yujongin <48309991+yujongin@users.noreply.github.com>
2025-02-25 15:43:16 +0900

    0225push

--- Operation_Playroom/Assets/@Jongin/Scripts/Timmy/MoveTimmy.cs
+++ Operation_Playroom/Assets/@Jongin/Scripts/Timmy/MoveTimmy.cs
@@ -8,22 +8,51 @@ public class MoveTimmy : MonoBehaviour
     ETimmyState timmyState = ETimmyState.Sleep;
 
     int pathIndex = 0;
+
+    NavMeshAgent agent;
+    Animator animator;
+    bool isMove = false;
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.G))
+        if (Input.GetKeyDown(KeyCode.G))
         {
             MoveToPath(pathIndex);
+            isMove = true;
+            animator.SetTrigger("Walk");
         }
+
+        if (isMove)
+        {
+            if (HasReachedDestination())
+            {
+                animator.SetTrigger("Lifting");
+                //lifting animation 
+
+                isMove = false;
+            }
+        }
+        Debug.Log(HasReachedDestination());
     }
 
     void MoveToPath(int index)
     {
         GetComponent<NavMeshAgent>().SetDestination(path[index].position);
         pathIndex++;
+    }
+
+    bool HasReachedDestination()
+    {
+        // 에이전트가 경로를 가지고 있고, 남은 거리가 정지 거리보다 작으면 도착한 것으로 판단
+        if (!agent.pathPending && agent.remainingDistance <= agent.stoppingDistance)
+        {
+            return !agent.hasPath || agent.velocity.sqrMagnitude == 0f;
+        }
+        return false;
     }
 }
